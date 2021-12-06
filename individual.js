d8ffc1d3293a9e4cea9956d3a0ea382e561a9486
undef125 <86185111+undef125@users.noreply.github.com>
2021-12-05 19:27:51 -0800

    ^_^

--- individual.js
+++
@@ -1,9 +0,0 @@
-// import {selectedProvince} from './app.js'
-
-console.log(globalThis.selectedProvince);
-
-const districtWrapper = document.querySelector('.district-wrapper');
-const headTex = document.querySelector('.heading-dis');
-const districtHolder = document.querySelector('.district-holder');
-
-
